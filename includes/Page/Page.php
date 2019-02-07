aca3ab5ff75850473ec333238a7ee1d8cc8c689a
Philippe AUTIER <philippe.autier@leoo.fr>
2019-02-07 23:37:17 +0100

    [+] fix : page and add other stuff

--- includes/Page/Page.php
+++ includes/Page/Page.php
@@ -5,10 +5,17 @@
  * @package WPBushido
  */
 
-namespace WPBushido\Client;
+namespace WPBushido\Page;
 
 class Page
 {
+
+    /**
+     *
+     * @var $checkSeoFields
+     */
+    public static $checkSeoFields = array('title', 'description', 'keywords', 'image', 'main_title');
+
     /**
      * Render Template to check 404 error
      *
@@ -113,6 +120,12 @@ class Page
         return $arg;
     }
 
+    /**
+     * Build menu from WP Page
+     *
+     * @param int $parent
+     * @return array $returnPages with all menu items
+     */
     public static function getMenuPages($parent = 0)
     {
         // Get Main Page
@@ -151,6 +164,12 @@ class Page
         return $returnPages;
     }
 
+    /**
+     * Get Post template name
+     *
+     * @param int $post_id
+     * @return string $tpl name of the page template
+     */
     public static function getTplName($post_id)
     {
         $tpl = get_page_template_slug($post_id);
@@ -161,5 +180,108 @@ class Page
             $tpl = false;
         }
         return $tpl;
+    }
+
+    /**
+     * Get Seo information
+     *
+     * @param array $context : timber context
+     * @param int $forceParent
+     * @return array $return all seo information
+     */
+    public static function getSeo($context, $forceParent = 0)
+    {
+        if (!isset($context['page_seo'])) {
+            $return = array();
+        }
+        else {
+            $return = $context['page_seo'];
+        }
+        $pageId = false;
+        if ($forceParent) {
+            $pageId = $forceParent;
+        }
+        else if (isset($context['page_id']) && !empty($context['page_id'])) {
+            $pageId = $context['page_id'];
+        }
+        if ($pageId) {
+            $acfFields = get_fields($pageId);
+            if (isset($acfFields['seo_parent_synchronisation']) && $acfFields['seo_parent_synchronisation'] == true && $pageId != intval(get_option('page_on_front'))) {
+                $parentID = wp_get_post_parent_id($pageId);
+                if ($parentID == 0) {
+                    $parentID = intval(get_option('page_on_front'));
+                }
+                $return = self::getSeo($context, $parentID);
+            }
+            else {
+                foreach (self::$checkSeoFields as $field) {
+                    if (isset($acfFields['seo_' . $field]) && !empty($acfFields['seo_' . $field])) {
+                        $return['seo_' . $field] = self::checkSeoString($acfFields['seo_' . $field], $context);
+                    }
+                    if (isset($acfFields['seo_' . $field . '_detail']) && !empty($acfFields['seo_' . $field . '_detail'])) {
+                        $return['seo_' . $field . '_detail'] = self::checkSeoString($acfFields['seo_' . $field . '_detail'], $context);
+                    }
+                    if (isset($acfFields['seo_' . $field . '_form']) && !empty($acfFields['seo_' . $field . '_form'])) {
+                        $return['seo_' . $field . '_form'] = self::checkSeoString($acfFields['seo_' . $field . '_form'], $context);
+                    }
+                    if (isset($acfFields['seo_' . $field . '_'.$context['level']]) && !empty($acfFields['seo_' . $field . '_'.$context['level']])) {
+                        $return['seo_' . $field . '_'.$context['level']] = self::checkSeoString($acfFields['seo_' . $field . '_'.$context['level']], $context);
+                    }
+                    // if (isset($acfFields['seo_' . $field . '_']) && !empty($acfFields['seo_' . $field . '_dealer'])) {
+                    //     $return['seo_' . $field . '_dealer'] = self::checkSeoString($acfFields['seo_' . $field . '_dealer'], $context);
+                    // }
+                }
+            }
+        }
+        return $return;
+    }
+
+    /**
+     * Process on SEO data
+     *
+     * @param string $string : string SEO to proceed
+     * @param array $context : timber context
+     * @return string $string string with all changes
+     */
+    public static function checkSeoString($string, $context)
+    {
+        $arrayStopWords = array(
+            'item'
+        );
+        $findStepWordsExpression = array();
+        preg_match_all('(\#[^#]+\#)', $string, $matches);
+        if (isset($matches[0])) {
+            $matches = $matches[0];
+        }
+        foreach ($matches as $match) {
+            $cleanStepWords = $match;
+            $cleanStepClean = '';
+            preg_match('(\(.*\))', $match, $matchesInto);
+            foreach ($matchesInto as $matchesIntoItem) {
+                $cleanStepWords = str_replace(array($matchesIntoItem, '#'), array('',''), $match);
+                $cleanStepClean = str_replace(array('(',')'), array('', ''), $matchesIntoItem);
+            }
+            $findStepWordsExpression[] = array('stepword' => $cleanStepWords, 'expression' => $cleanStepClean, 'original' => $match);
+        }
+        foreach ($findStepWordsExpression as $stepWordK) {
+            $stepWord = $stepWordK['stepword'];
+            $expressionWord = $stepWordK['expression'];
+            $originalWords = $stepWordK['original'];
+            $replaceValue = '';
+            if (in_array($stepWord, $arrayStopWords)) {
+                $value = '';
+                if (!empty($value)) {
+                    if (!empty($expressionWord)) {
+                        $replaceValue = str_replace('%s', $value, $expressionWord);
+                    } else {
+                        $replaceValue = $value;
+                    }
+                }
+            }
+            $string = str_replace($originalWords, $replaceValue, $string);
+        }
+        $string = trim($string);
+        $string = str_replace('  ', ' ', $string);
+        return $string;
     }
 }
